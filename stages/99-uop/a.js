e9a8a447c23c819bfadfffaa555d1541bb2e93ec
Jacek Kopecky <jacek@jacek.cz>
2020-03-06 11:57:35 +0000

    add play button

--- stages/99-uop/a.js
+++ stages/99-uop/a.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", () => {
   const audio = new Audio('http://goo.gl/WppRG9');
   window.logo.addEventListener( "animationstart", () => audio.play() );
+  window.play.addEventListener( "click", () => document.body.classList.add('playing'));
 });
