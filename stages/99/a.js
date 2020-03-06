18390fc393815d9266e5cbe0105365176300bdd9
Rich Boakes <rjb@port.ac.uk>
2020-03-06 17:16:13 +0000

    borked audio fade

--- stages/99/a.js
+++ stages/99/a.js
@@ -1,5 +1,22 @@
+let audio;
+
 window.addEventListener("load", () => {
-  const audio = new Audio('http://goo.gl/WppRG9');
+  audio = new Audio('http://goo.gl/WppRG9');
   window.logo.addEventListener( "animationstart", () => audio.play() );
-  window.play.addEventListener( "click", () => document.body.classList.add('playing'));
+  window.play.addEventListener( "click", () => {
+    document.body.classList.add('playing');
+    window.setTimeout(startFade, 780);
+  });
 });
+
+function startFade() {
+    const doFade = setInterval(() => {
+      if (audio.volume > 0) {
+        audio.volume -= 0.01;
+      } else {
+        clearInterval(doFade);
+      }
+    }, 20
+);
+}
+
