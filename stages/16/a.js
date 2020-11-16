6891c904957b746efb84ae9a4f7eefab0b0d63e6
Jacek Kopecky <jacek@jacek.cz>
2020-11-16 12:30:04 +0000

    empty starting point

--- stages/16/a.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("load", () => {
-  const audio = new Audio('https://ia803204.us.archive.org/16/items/StarWarsThemeSongByJohnWilliams/Star%20Wars%20Theme%20Song%20By%20John%20Williams.mp3');
-  window.logo.addEventListener( "animationstart", () => audio.play() );
-});
